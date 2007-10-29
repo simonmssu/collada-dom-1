7f99091e001dd5fa2f4a27ed1c83cb4a5f24b6c7
steve314 <steve314@dfea1ed5-6b0d-0410-973e-e49e28f48b26>
2007-10-29 00:39:56 +0000

    Copying the DOM trunk to the Windows unit test branch.
    git-svn-id: https://collada-dom.svn.sourceforge.net/svnroot/collada-dom/branches/winUnitTests@269 dfea1ed5-6b0d-0410-973e-e49e28f48b26

---
+++ COLLADA_DOM/include/dae/daeIDRef.h
@@ -0,0 +1,294 @@
+/*
+ * Copyright 2006 Sony Computer Entertainment Inc.
+ *
+ * Licensed under the SCEA Shared Source License, Version 1.0 (the "License"); you may not use this 
+ * file except in compliance with the License. You may obtain a copy of the License at:
+ * http://research.scea.com/scea_shared_source_license.html
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License 
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+ * implied. See the License for the specific language governing permissions and limitations under the 
+ * License. 
+ */
+
+#ifndef __DAE_IDREF_H__
+#define __DAE_IDREF_H__
+
+#include <string>
+#include <dae/daeTypes.h>
+#include <dae/daeElement.h>
+
+/**
+ * The @c daeIDRef is a simple class designed to aid in the parsing and resolution of
+ * ID references inside of COLLADA elements.
+ * A @c daeIDRef is created for every IDREF data type in the COLLADA schema.
+ * It also has the capability to attempt to resolve this reference
+ * into a @c daeElement.  If a @c daeIDRef is stored within a @c daeElement it fills
+ * in its container field to point to the containing element.
+ *
+ * The main API is the @c daeIDRef::resolveElement() will use a @c daeIDRefResolver
+ * to search for the @c daeElement inside of a @c daeDatabase.
+ *
+ */
+class daeIDRef
+{
+public:
+	/**
+	 * An enum describing the status of the ID resolution process.
+	 */
+	DLLSPEC enum ResolveState{
+		/** No ID specified */
+		id_empty,
+		/** ID specified but not resolved */
+		id_loaded,
+		/** ID resolution pending */
+		id_pending,
+		/** ID resolved correctly */
+		id_success,
+		/** Resolution failed because ID was not found */
+		id_failed_id_not_found,
+		/** Resolution failed because ID was invalid */
+		id_failed_invalid_id,
+		/** Resoltion failed due to invalid reference */
+		id_failed_invalid_reference,
+		/** Resolution failed due to an external error */
+		id_failed_externalization,
+		/** Resolution failed because we don't have a document in which to search for the element.
+				This means you probably forgot to set a container element. */
+		id_failed_no_document
+	};
+	
+private:	
+	/** ID used to refer to another element */
+	std::string id;
+
+	/** Reference to the actual element the ID refers to */
+	mutable daeElementRef element;
+
+	/** Element that owns this ID (if any) */
+	daeElement* container;
+
+public:
+	/**
+	 * Simple Constructor
+	 */
+	DLLSPEC daeIDRef();
+
+	/**
+	 * Constructs an id reference via a string, using @c setID(); loads the status.
+	 * @param id ID to construct a reference for, passed to @c setID() automatically.
+	 */
+	DLLSPEC daeIDRef(daeString id);
+	
+	/**
+	 * Constructs a new id reference by copying an existing one. 
+	 * @param constructFromIDRef @c daeIDRef to copy into this one.
+	 */
+	DLLSPEC daeIDRef(const daeIDRef& constructFromIDRef);
+
+	/**
+	 * Gets the ID string
+	 * @return Returns the full ID string from <tt><i>id.</i></tt> 
+	 */
+	DLLSPEC daeString getID() const;
+
+	/**
+	 * Copies <tt><i>ID</i></tt> into the  <tt><i>id	</i></tt> data member.
+	 * After the call to @c setID(), the <tt><i>state</i></tt> is set to @c id_loaded
+	 * @param ID String to use to configure this @c daeIDRef.
+	 */
+	DLLSPEC void setID(daeString ID);
+
+	/** 
+	 * Gets the element that this URI resolves to in memory.
+	 * @return Returns a ref to the element.
+	 */
+	DLLSPEC daeElementRef getElement() const;
+
+	/** 
+	 * Sets the element that this URI resolves to in memory.
+	 * @param newref A ref to the element.
+	 */
+	DLLSPEC void setElement(daeElementRef newref);
+
+	/**
+	 * Gets the resolve state of the URI.
+	 * @return Returns the current state.
+	 * @note This will be removed when daeURI starts managing its state internally.
+	 */
+	DLLSPEC ResolveState getState() const;
+
+	/**
+	 * Gets a pointer to the @c daeElement that contains this URI.
+	 * @return Returns the pointer to the containing daeElmement.
+	 */
+	DLLSPEC daeElement* getContainer() const;
+
+	/**
+	 * Sets the pointer to the @c daeElement that contains this URI.
+	 * @param cont Pointer to the containing @c daeElmement.
+	 */
+	DLLSPEC void setContainer(daeElement* cont);
+
+	/**
+	 * Outputs all components of this @c daeIDRef to stderr.
+	 */
+	DLLSPEC void print();
+
+	/**
+	 * Resets this @c daeIDRef; frees all string references
+	 * and returns <tt><i>state</i></tt> to @c empty.
+	 */
+	DLLSPEC void reset();
+
+	/**
+	 * Initializes the @c daeIDREf, setting <tt><i>id, element,</i></tt>  and <tt><i>container</i></tt> to NULL.
+	 */
+	DLLSPEC void initialize();
+
+	/**
+	 * Comparison operator.
+	 * @return Returns true if URI's are equal.
+	 */
+	DLLSPEC bool operator==(const daeIDRef& other) const;
+
+	/**
+	 * Assignment operator.
+	 * @return Returns a reference to this object.
+	 */
+	DLLSPEC daeIDRef &operator=( const daeIDRef& other);
+
+	// These methods are only provided for backwards compatibility. Use the listed alternatives.
+	DLLSPEC daeIDRef &get( daeUInt idx ); // Never should have existed. No alternative.
+	DLLSPEC size_t getCount() const; // Never should have existed. No alternative.
+	DLLSPEC daeIDRef& operator[](size_t index); // Never should have existed. No alternative.
+	DLLSPEC void resolveElement( daeString typeNameHint = NULL ); // Call getElement. No separate "resolve" step needed.
+	DLLSPEC void resolveID(); // Never should have existed. No alternative.
+	DLLSPEC void validate(); // Never should have existed. No alternative.
+	DLLSPEC void copyFrom(const daeIDRef& from); // Use the assignment operator instead.
+};
+
+class daeIDRefResolver;
+typedef daeTArray<daeIDRefResolver*> daeIDRefResolverPtrArray;
+
+/**
+ * The @c daeIDRefResolver class is the plugin point for @c daeIDRef resolution.
+ * This class is an abstract base class that defines an interface for
+ * resolving @c daeIDRefs.
+ * All instances of @c daeIDRefResolvers are tracked centrally.
+ * Every @c daeIDRef is passed through this list of @c daeIDRefResolvers for resolution.
+ * The list is ordered on a first come, first serve basis, and resolution
+ * terminates after any resolver instance is able to resolve the ID.
+ */
+class daeIDRefResolver
+{
+public:
+	/**
+	 * Constructor; base constructor appends @c this to <tt><i>_KnownResolvers</i></tt> list.
+	 */
+	DLLSPEC daeIDRefResolver();
+
+	/**
+	 * Destructor
+	 */
+	virtual DLLSPEC ~daeIDRefResolver();
+	
+//Contributed by Nus - Wed, 08 Nov 2006
+	/**
+	 * Initialize ID reference solver
+	 */
+	static void initializeIDRefSolver(void);
+
+	/**
+	 * Terminate ID reference solver
+	 */
+	static void terminateIDRefSolver(void);
+//-------------------------
+
+protected:
+	static daeIDRefResolverPtrArray* _KnownResolvers;
+	
+public:
+	/**
+	 * Iterates through known resolvers calling resolveElement().
+	 * @param id The ID of the element to find.
+	 * @param docURI The URI of the document containing the element.
+	 * @param result A ResolveState value indicating the result.
+	 * @return Returns a daeElement with matching ID, if one is found.
+	 */
+	static DLLSPEC daeElement* attemptResolveElement(daeString id, 
+																									 daeString docURI, 
+																									 daeIDRef::ResolveState* result = NULL);
+
+public: // Abstract Interface
+	/**
+	 * Provides an abstract interface to convert a @c daeIDRef into a @c daeElement.
+	 * @param id The ID of the element to find.
+	 * @param docURI The URI of the document containing the element.
+	 * @param result A ResolveState value indicating the result.
+	 * @return Returns a daeElement with matching ID, if one is found.
+	 */
+	virtual DLLSPEC daeElement* resolveElement(daeString id,
+																						 daeString docURI,
+																						 daeIDRef::ResolveState* result = NULL) = 0;
+
+	/**
+	 * Gets the name of this resolver.
+	 * @return Returns the string name.
+	 */
+	virtual DLLSPEC daeString getName() = 0;
+
+};
+
+class daeDatabase;
+
+/**
+ * The @c daeDefaultIDRefResolver resolves a @c daeIDRef by checking with a database.
+ * It is a concrete implementation for @c daeIDRefResolver.
+ */
+class daeDefaultIDRefResolver : public daeIDRefResolver
+{
+public:
+	/**
+	 * Constructor
+	 * @param database @c daeDatabase for this implementation.
+	 */
+	DLLSPEC daeDefaultIDRefResolver();
+
+	/**
+	 * Destructor
+	 */
+	DLLSPEC ~daeDefaultIDRefResolver();
+
+	void setDatabase( daeDatabase *db ) { _database = db; }
+
+protected:
+	daeDatabase* _database;
+	
+public: // Abstract Interface
+	/*
+	 * Implements base class abstract routine from @c daeIDRefResolver.
+	 */
+	virtual DLLSPEC daeElement* resolveElement(daeString id,
+																						 daeString docURI,
+																						 daeIDRef::ResolveState* result = NULL);
+	
+	/*
+	 * Implements base class abstract routine from @c daeIDRefResolver.
+	 */
+	virtual DLLSPEC daeString getName();
+};
+
+#endif //__DAE_IDREF_H__
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ COLLADA_DOM/src/dae/daeTinyXMLPlugin.cpp
@@ -0,0 +1,287 @@
+/*
+ * Copyright 2007 Sony Computer Entertainment Inc.
+ *
+ * Licensed under the SCEA Shared Source License, Version 1.0 (the "License"); you may not use this 
+ * file except in compliance with the License. You may obtain a copy of the License at:
+ * http://research.scea.com/scea_shared_source_license.html
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License 
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+ * implied. See the License for the specific language governing permissions and limitations under the 
+ * License. 
+ */
+
+// The user can choose whether or not to include TinyXML support in the DOM. Supporting TinyXML will
+// require linking against it. By default TinyXML support isn't included.
+#if defined(DOM_INCLUDE_TINYXML)
+
+#if defined(DOM_DYNAMIC) && defined(WIN32)
+#pragma comment(lib, "tinyxml.lib")
+#endif
+
+#if defined(WIN32)
+#pragma warning(disable: 4100) // warning C4100: 'element' : unreferenced formal parameter
+#endif
+
+#include <string>
+#include <tinyxml.h>
+#include <dae.h>
+#include <dom.h>
+#include <dae/daeMetaElement.h>
+#include <dae/daeErrorHandler.h>
+#include <dae/daeMetaElementAttribute.h>
+#include <dae/daeTinyXMLPlugin.h>
+
+namespace {
+	daeInt getCurrentLineNumber(TiXmlElement* element) {
+		return -1;
+	}
+}
+
+daeTinyXMLPlugin::daeTinyXMLPlugin()
+{
+  m_doc = NULL;
+}
+
+daeTinyXMLPlugin::~daeTinyXMLPlugin()
+{
+}
+
+daeTArray<std::string> daeTinyXMLPlugin::getSupportedProtocols() {
+	daeTArray<std::string> protocols;
+	protocols.append("file");
+	return protocols;
+}
+
+daeInt daeTinyXMLPlugin::setOption( daeString option, daeString value )
+{
+	return DAE_ERR_INVALID_CALL;
+}
+
+daeString daeTinyXMLPlugin::getOption( daeString option )
+{
+	return NULL;
+}
+
+void daeTinyXMLPlugin::getProgress(daeInt* bytesParsed,
+						 daeInt* lineNumber,
+						 daeInt* totalBytes,
+						 daeBool reset)
+{
+	if (totalBytes)
+		*totalBytes = 0; // Not available
+}
+
+
+daeElementRef daeTinyXMLPlugin::readFromFile(const daeURI& uri) {
+	// !!!steveT: Replace this with real URI to file path code, and update the resolver to only handle the file scheme
+	TiXmlDocument doc;
+	char file[512];
+	uri.getPath(file, 512);
+#if defined(WIN32)
+	doc.LoadFile(&file[1]);
+#else
+	doc.LoadFile(&file[0]);
+#endif
+	if (!doc.RootElement()) {
+		daeErrorHandler::get()->handleError((std::string("Failed to open ") + uri.getURI() +
+																				 " in daeTinyXMLPlugin::readFromFile\n").c_str());
+		return NULL;
+	}
+	return readElement(doc.RootElement(), NULL);
+}
+
+daeElementRef daeTinyXMLPlugin::readFromMemory(daeString buffer, const daeURI& baseUri) {
+	TiXmlDocument doc;
+	doc.Parse(buffer);
+	if (!doc.RootElement()) {
+		daeErrorHandler::get()->handleError("Failed to open XML document from memory buffer in "
+																				"daeTinyXMLPlugin::readFromMemory\n");
+		return NULL;
+	}
+	return readElement(doc.RootElement(), NULL);
+}
+
+daeElementRef daeTinyXMLPlugin::readElement(TiXmlElement* tinyXmlElement, daeElement* parentElement) {
+	std::vector<attrPair> attributes;
+	for (TiXmlAttribute* attrib = tinyXmlElement->FirstAttribute(); attrib != NULL; attrib = attrib->Next())
+		attributes.push_back(attrPair(attrib->Name(), attrib->Value()));
+		
+	daeElementRef element = beginReadElement(parentElement, tinyXmlElement->Value(),
+																					 attributes, getCurrentLineNumber(tinyXmlElement));
+	if (!element) {
+		// We couldn't create the element. beginReadElement already printed an error message.
+		return NULL;
+	}
+
+  if (tinyXmlElement->GetText() != NULL)
+		readElementText(element, tinyXmlElement->GetText(), getCurrentLineNumber(tinyXmlElement));
+  
+  // Recurse children
+  for (TiXmlElement* child = tinyXmlElement->FirstChildElement(); child != NULL; child = child->NextSiblingElement())
+    element->placeElement(readElement(child, element));
+
+	return element;
+}
+
+daeInt daeTinyXMLPlugin::write(daeURI *name, daeDocument *document, daeBool replace)
+{
+	// Make sure database and document are both set
+	if (!database)
+		return DAE_ERR_INVALID_CALL;
+	if(!document)
+		return DAE_ERR_COLLECTION_DOES_NOT_EXIST;
+
+	// !!!steveT: Do a decent URI to file path conversion
+	// Extract just the file path from the URI
+	daeFixedName finalname;
+	if (!name->getPath(finalname,sizeof(finalname)))
+	{
+		daeErrorHandler::get()->handleError( "can't get path in write\n" );
+		return DAE_ERR_BACKEND_IO;
+	}
+	// If replace=false, don't replace existing files
+	if(!replace)
+	{
+		// Using "stat" would be better, but it's not available on all platforms
+#ifdef WIN32
+		FILE *tempfd = fopen(finalname+1,"r");
+#else
+		FILE *tempfd = fopen(finalname, "r");
+#endif
+		if(tempfd != NULL)
+		{
+			// File exists, return error
+			fclose(tempfd);
+			return DAE_ERR_BACKEND_FILE_EXISTS;
+		}
+		fclose(tempfd);
+	}
+	
+  m_doc = new TiXmlDocument(name->getURI());
+  if (m_doc)
+  {
+    m_doc->SetTabSize(4);
+
+   	TiXmlDeclaration* decl = new TiXmlDeclaration( "1.0", "", "" );  
+	  m_doc->LinkEndChild( decl ); 
+
+    writeElement(document->getDomRoot());
+
+#ifdef WIN32
+		m_doc->SaveFile(finalname+1);
+#else
+    m_doc->SaveFile(finalname);
+#endif
+    delete m_doc;
+    m_doc = NULL;
+  }
+	return DAE_OK;
+}
+
+void daeTinyXMLPlugin::writeElement( daeElement* element )
+{
+	daeIntegrationObject* _intObject = element->getIntObject();
+	daeMetaElement* _meta = element->getMeta();
+	if(_intObject)
+	{
+		// added in response to bug 478
+		_intObject->toCOLLADAChecked();
+		_intObject->toCOLLADAPostProcessChecked();
+	}
+
+  if (!_meta->getIsTransparent() ) 
+  {
+		TiXmlElement* tiElm = new TiXmlElement( element->getElementName() );  
+        
+		if (m_elements.empty() == true) {
+				m_doc->LinkEndChild(tiElm);
+			} else {
+			TiXmlElement* first = m_elements.front();
+			first->LinkEndChild(tiElm);
+		}
+		m_elements.push_front(tiElm);
+
+		daeMetaAttributeRefArray& attrs = _meta->getMetaAttributes();
+		
+		int acnt = (int)attrs.getCount();
+		
+		for(int i=0;i<acnt;i++) 
+    {
+			writeAttribute( attrs[i], element );
+		}
+	}
+	writeValue(element);
+	
+	daeElementRefArray children;
+	element->getChildren( children );
+	for ( size_t x = 0; x < children.getCount(); x++ ) 
+  {
+		writeElement( children.get(x) );
+	}
+
+	/*if (_meta->getContents() != NULL) {
+		daeElementRefArray* era = (daeElementRefArray*)_meta->getContents()->getWritableMemory(element);
+		int elemCnt = (int)era->getCount();
+		for(int i = 0; i < elemCnt; i++) {
+			daeElementRef elem = (daeElementRef)era->get(i);
+			if (elem != NULL) {
+				writeElement( elem );
+			}
+		}
+	}
+	else
+	{
+		daeMetaElementAttributeArray& children = _meta->getMetaElements();
+		int cnt = (int)children.getCount();
+		for(int i=0;i<cnt;i++) {
+			daeMetaElement *type = children[i]->getElementType();
+			if ( !type->getIsAbstract() ) {
+				for (int c = 0; c < children[i]->getCount(element); c++ ) {
+					writeElement( *(daeElementRef*)children[i]->get(element,c) );
+				}
+			}
+		}
+	}*/
+	if (!_meta->getIsTransparent() ) 
+  {
+    m_elements.pop_front();
+	}
+}
+
+
+void daeTinyXMLPlugin::writeValue( daeElement* element )
+{
+	if (daeMetaAttribute* attr = element->getMeta()->getValueAttribute()) {
+		std::ostringstream buffer;
+		attr->memoryToString(element, buffer);
+		std::string s = buffer.str();
+		if (!s.empty())
+			m_elements.front()->LinkEndChild( new TiXmlText(buffer.str().c_str()) );
+	}
+}
+
+void daeTinyXMLPlugin::writeAttribute( daeMetaAttribute* attr, daeElement* element )
+{
+	//don't write if !required and is set && is default
+	if ( !attr->getIsRequired() ) {
+		//not required
+		if ( !element->isAttributeSet( attr->getName() ) ) {
+			//early out if !value && !required && !set
+			return;
+		}
+			
+		//is set
+		//check for default suppression
+		if (attr->compareToDefault(element) == 0) {
+			// We match the default value, so exit early
+			return;
+		}
+	}
+
+	std::ostringstream buffer;
+	attr->memoryToString(element, buffer);
+	m_elements.front()->SetAttribute(attr->getName(), buffer.str().c_str());
+}
+
+#endif // DOM_INCLUDE_TINYXML
